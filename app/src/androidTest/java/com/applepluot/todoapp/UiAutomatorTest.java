f3bb36e32c770c2fc6c08209b634802b3ab362ba
Apple Chow <apple@uber.com>
2016-04-07 19:47:12 -0700

    Updated tests.

--- app/src/androidTest/java/com/applepluot/todoapp/UiAutomatorTest.java
+++ app/src/androidTest/java/com/applepluot/todoapp/UiAutomatorTest.java
@@ -2,6 +2,8 @@ package com.applepluot.todoapp;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.graphics.Rect;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.filters.SdkSuppress;
@@ -10,6 +12,7 @@ import android.support.test.uiautomator.By;
 import android.support.test.uiautomator.UiDevice;
 import android.support.test.uiautomator.UiObject2;
 import android.support.test.uiautomator.Until;
+import android.test.suitebuilder.annotation.LargeTest;
 import android.util.Log;
 
 import org.junit.Before;
@@ -25,14 +28,38 @@ import static org.junit.Assert.assertThat;
 
 @RunWith(AndroidJUnit4.class)
 @SdkSuppress(minSdkVersion = 18)
+@LargeTest
 public class UiAutomatorTest {
+    /**
+     * The target app package.
+     */
+    private static final String TARGET_PACKAGE =
+            InstrumentationRegistry.getTargetContext().getPackageName();
     private static final String TODO_APP_PACKAGE
             = "com.applepluot.todoapp";
-    private static final int LAUNCH_TIMEOUT = 10000;
+
+    private static final int LAUNCH_TIMEOUT = 15000;
     private static final int DEFAULT_TIMEOUT = 3000;
     private static final String TAG = UiAutomatorTest.class.getSimpleName();
     SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss");
     private UiDevice mDevice;
+    private Context mContext;
+
+    /**
+     * Uses package manager to find the package name of the device launcher. Usually this package
+     * is "com.android.launcher" but can be different at times. This is a generic solution which
+     * works on all platforms.
+     */
+    private String getLauncherPackageName() {
+        // Create launcher Intent
+        final Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+
+        // Use PackageManager to get the launcher package name
+        PackageManager pm = InstrumentationRegistry.getContext().getPackageManager();
+        ResolveInfo resolveInfo = pm.resolveActivity(intent, PackageManager.MATCH_DEFAULT_ONLY);
+        return resolveInfo.activityInfo.packageName;
+    }
 
     @Before
     public void startMainActivityFromHomeScreen() {
@@ -43,33 +70,46 @@ public class UiAutomatorTest {
         mDevice.pressHome();
 
         // Wait for launcher
-        final String launcherPackage = mDevice.getLauncherPackageName();
+        final String launcherPackage = getLauncherPackageName();
         assertThat(launcherPackage, notNullValue());
         mDevice.wait(Until.hasObject(By.pkg(launcherPackage).depth(0)), LAUNCH_TIMEOUT);
 
         // Launch the app
         Context context = InstrumentationRegistry.getContext();
+        System.out.println("TARGET: " + TARGET_PACKAGE);
         final Intent intent = context.getPackageManager()
-                .getLaunchIntentForPackage(TODO_APP_PACKAGE);
+                .getLaunchIntentForPackage(TARGET_PACKAGE);
         // Clear out any previous instances
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
         context.startActivity(intent);
 
         // Wait for the app to appear
-        mDevice.wait(Until.hasObject(By.pkg(TODO_APP_PACKAGE).depth(0)), LAUNCH_TIMEOUT);
+        mDevice.wait(Until.hasObject(By.pkg(TARGET_PACKAGE).depth(0)), LAUNCH_TIMEOUT);
+        mContext = InstrumentationRegistry.getTargetContext();
+    }
+
+    @Test
+    public void checkPreconditions() {
+        assertThat(mDevice, notNullValue());
     }
 
     @Test
     public void testAppLaunch() {
         Log.i(TAG, "testAppLaunch");
-        UiObject2 addButton = mDevice.findObject(By.res(TODO_APP_PACKAGE, "btnAdd"));
+        final String addButtonDescription = mContext.getString(R.string.add_button);
+        UiObject2 addButton = mDevice.findObject(By.desc(addButtonDescription));
         assertTrue(addButton.isEnabled());
+        UiObject2 addItemText = mDevice.wait(Until.findObject(
+                By.res(TODO_APP_PACKAGE, "etAddItemText")), DEFAULT_TIMEOUT);
+        assertTrue(addItemText.isEnabled());
     }
 
     @Test
     public void testAddAndRemoveItem() {
         Log.i(TAG, "testAddAndRemoveItem");
-        UiObject2 addButton = mDevice.findObject(By.res(TODO_APP_PACKAGE, "btnAdd"));
+        UiObject2 addButton = mDevice.wait(Until.findObject(
+                By.res(TODO_APP_PACKAGE, "btnAdd")), DEFAULT_TIMEOUT);
+        assertTrue(addButton.isEnabled());
         UiObject2 addItemText = mDevice.wait(Until.findObject(
                 By.res(TODO_APP_PACKAGE, "etAddItemText")), DEFAULT_TIMEOUT);
         String newItemText = sdf.format(Calendar.getInstance().getTime());
